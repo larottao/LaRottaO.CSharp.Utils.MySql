57c406c78a78aeebdd39f79c0bebd2c07ba01c52
larottao <59802452+larottao@users.noreply.github.com>
2022-10-11 15:51:37 -0500

    Bug fix ExecuteStoredProcedure should return List<Dictionary<String, Object>> instead of List<Dictionary<String, String>

--- LaRottaO.CSharp.MySqlUtilities/ExecuteStoredProcedure.cs
+++ LaRottaO.CSharp.MySqlUtilities/ExecuteStoredProcedure.cs
@@ -10,7 +10,7 @@ namespace LaRottaO.CSharp.MySqlUtilities
 {
     public class ExecuteStoredProcedure
     {
-        public async Task<Tuple<Boolean, String, List<Dictionary<String, String>>>> executeStoredProcedure(String argConnString, String argStoredProcedureName, Dictionary<String, object> argInputParametersList, List<String> argOutputParametersList, int argTimeOutMs)
+        public async Task<Tuple<Boolean, String, List<Dictionary<String, Object>>>> executeStoredProcedure(String argConnString, String argStoredProcedureName, Dictionary<String, object> argInputParametersList, List<String> argOutputParametersList, int argTimeOutMs)
         {
             MySqlConnection MmSqlConnection = new MySqlConnection(argConnString);
             MySqlCommand mySqlCommand = new MySqlCommand();
@@ -38,14 +38,14 @@ namespace LaRottaO.CSharp.MySqlUtilities
                     mySqlCommand.Parameters.Add(new MySqlParameter(inputParameter.Key, inputParameter.Value));
                 }
 
-                List<Dictionary<String, String>> totalOutputRows = new List<Dictionary<String, String>>();
-
-                Dictionary<String, String> singleOutputRow = new Dictionary<String, String>();
+                List<Dictionary<String, Object>> totalOutputRows = new List<Dictionary<String, Object>>();
 
                 using (MySqlDataReader mySqlDataReader = mySqlCommand.ExecuteReader())
                 {
                     while (mySqlDataReader.Read())
                     {
+                        Dictionary<String, Object> singleOutputRow = new Dictionary<String, Object>();
+
                         foreach (String outputParameter in argOutputParametersList)
                         {
                             singleOutputRow.Add(outputParameter, mySqlDataReader[outputParameter].ToString());
@@ -55,11 +55,11 @@ namespace LaRottaO.CSharp.MySqlUtilities
                     }
                 }
 
-                return new Tuple<Boolean, String, List<Dictionary<String, String>>>(true, Constants.MYSQL_SUCCESS, totalOutputRows);
+                return new Tuple<Boolean, String, List<Dictionary<String, Object>>>(true, Constants.MYSQL_SUCCESS, totalOutputRows);
             }
             catch (Exception ex)
             {
-                return new Tuple<Boolean, String, List<Dictionary<String, String>>>(false, Constants.MYSQL_ERROR + ex.Message, new List<Dictionary<String, String>>());
+                return new Tuple<Boolean, String, List<Dictionary<String, Object>>>(false, Constants.MYSQL_ERROR + ex.Message, new List<Dictionary<String, Object>>());
             }
             finally
             {
